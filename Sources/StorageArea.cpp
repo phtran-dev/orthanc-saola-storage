90f830507205245dc1f29d34fb16eaff7e088e97
phtran <phtran.dev@gmail.com>
2024-12-13 09:43:48 +0700

    Add more logic when create attachments

--- Sources/StorageArea.cpp
+++ Sources/StorageArea.cpp
@@ -31,6 +31,7 @@
 #include <Logging.h>
 
 #include <boost/filesystem.hpp>
+#include <boost/thread.hpp>
 
 static boost::filesystem::path GetPathInternal(const std::string &root,
                                                const std::string &uuid)
@@ -149,15 +150,23 @@ static void CreateOrthancBuffer(OrthancPluginMemoryBuffer64 *target,
 void StorageArea::ReadWholeFromPath(OrthancPluginMemoryBuffer64 *target,
                                     const std::string &path)
 {
+  Orthanc::Toolbox::ElapsedTimer timer;
+  LOG(INFO) << "StorageArea::ReadWholeFromPath path \"" << path << "\"";
+
   std::string content;
   Orthanc::SystemToolbox::ReadFile(content, path);
   CreateOrthancBuffer(target, content);
+
+  LOG(INFO) << "StorageArea::ReadWholeFromPath path \"" << path << "\" (" << timer.GetHumanTransferSpeed(true, target->size) << ")";
 }
 
 void StorageArea::ReadRangeFromPath(OrthancPluginMemoryBuffer64 *target,
                                     const std::string &path,
                                     uint64_t rangeStart)
 {
+  Orthanc::Toolbox::ElapsedTimer timer;
+  LOG(INFO) << "StorageArea::ReadRangeFromPath path \"" << path << "\" (range from: " << rangeStart << ")";
+
   std::string content;
   Orthanc::SystemToolbox::ReadFileRange(
       content, path, rangeStart, rangeStart + target->size, true);
@@ -170,6 +179,8 @@ void StorageArea::ReadRangeFromPath(OrthancPluginMemoryBuffer64 *target,
   {
     memcpy(target->data, content.c_str(), content.size());
   }
+
+  LOG(INFO) << "StorageArea::ReadRangeFromPath path \"" << path << "\" (" << timer.GetHumanTransferSpeed(true, target->size) << ")";
 }
 
 StorageArea::StorageArea(const std::string &root) : root_(root)
@@ -216,45 +227,94 @@ void StorageArea::Create(const std::string &uuid,
 
   boost::filesystem::path mount_path = CreateMountDirectory(uuid, content, size);
 
-  // Validate root directory existence and Make root directory
-  if (boost::filesystem::exists(root_path.parent_path()))
-  {
-    if (!boost::filesystem::is_directory(root_path.parent_path()))
-    {
-      LOG(ERROR) << "[SaolaStorageArea] Root directory: " << root_path.parent_path() << " is existed but not a directory";
-      // throw Orthanc::OrthancException(Orthanc::ErrorCode_DirectoryOverFile);
-    }
-  }
-  else
-  {
-    if (!boost::filesystem::create_directories(root_path.parent_path()))
-    {
-      LOG(ERROR) << "[SaolaStorageArea] Root directory: " << root_path.parent_path() << " cannot be created";
-      // throw Orthanc::OrthancException(Orthanc::ErrorCode_FileStorageCannotWrite);
-    }
-  }
+  Orthanc::Toolbox::ElapsedTimer timer;
+  LOG(INFO) << "StorageArea::Create creating attachment \"" << uuid << "\" to (root=" << root_path << ", mount_path=" << mount_path << ")";
 
-  // Validate mount directory existence and Make mount directory
-  if (boost::filesystem::exists(mount_path.parent_path()))
+  // In very unlikely cases, a thread could be deleting a
+  // directory while another thread needs it -> introduce 3 retries at 1 ms interval
+  int retryCount = 0;
+  const int maxRetryCount = 3;
+
+  while (retryCount < maxRetryCount)
   {
-    if (!boost::filesystem::is_directory(mount_path.parent_path()))
+    retryCount++;
+    if (retryCount > 1)
     {
-      LOG(ERROR) << "[SaolaStorageArea] Mount directory: " << mount_path.parent_path() << " is existed but not a directory";
-      // throw Orthanc::OrthancException(Orthanc::ErrorCode_DirectoryOverFile);
+      boost::this_thread::sleep(boost::posix_time::milliseconds(2 * retryCount + (rand() % 10)));
+      LOG(INFO) << "Retrying (" << retryCount << ") to create attachment \"" << uuid << ", root=" << root_path << ", mount_path=" << mount_path;
+    }
+
+    // Validate root directory existence and Make root directory
+    if (boost::filesystem::exists(root_path.parent_path()))
+    {
+      if (!boost::filesystem::is_directory(root_path.parent_path()))
+      {
+        LOG(ERROR) << "[SaolaStorageArea] Root directory: " << root_path.parent_path() << " is existed but not a directory";
+        throw Orthanc::OrthancException(Orthanc::ErrorCode_DirectoryOverFile);
+      }
+    }
+    else
+    {
+      try
+      {
+        boost::filesystem::create_directories(root_path.parent_path());
+      }
+      catch (boost::filesystem::filesystem_error &er)
+      {
+        if (er.code() == boost::system::errc::file_exists         // the last element of the parent_path is a file
+            || er.code() == boost::system::errc::not_a_directory) // one of the element of the parent_path is not a directory
+        {
+          throw Orthanc::OrthancException(Orthanc::ErrorCode_DirectoryOverFile,
+                                          std::string("[SaolaStorageArea] Parent Root directory: ") + root_path.parent_path().c_str() + " is a file"); // no need to retry this error
+        }
+        // ignore other errors and retry
+      }
+    }
+
+    // Validate mount directory existence and Make mount directory
+    if (boost::filesystem::exists(mount_path.parent_path()))
+    {
+      if (!boost::filesystem::is_directory(mount_path.parent_path()))
+      {
+        LOG(ERROR) << "[SaolaStorageArea] Mount directory: " << mount_path.parent_path() << " is existed but not a directory";
+        // throw Orthanc::OrthancException(Orthanc::ErrorCode_DirectoryOverFile);
+      }
+    }
+    else
+    {
+      try
+      {
+        boost::filesystem::create_directories(mount_path.parent_path());
+      }
+      catch (boost::filesystem::filesystem_error &er)
+      {
+        if (er.code() == boost::system::errc::file_exists         // the last element of the parent_path is a file
+            || er.code() == boost::system::errc::not_a_directory) // one of the element of the parent_path is not a directory
+        {
+          throw Orthanc::OrthancException(Orthanc::ErrorCode_DirectoryOverFile,
+                                          std::string("[SaolaStorageArea] Parent mount directory: ") + mount_path.parent_path().c_str() + " is a file"); // no need to retry this error
+        }
+        // ignore other errors and retry
+      }
+    }
+
+    try
+    {
+      Orthanc::SystemToolbox::WriteFile(mount_path.string().c_str(), mount_path.string().size(), root_path.string(), false);
+      Orthanc::SystemToolbox::WriteFile(content, size, mount_path.string(), false);
+      LOG(INFO) << "StorageArea::Create created attachment \"" << uuid << "\" (" << timer.GetHumanTransferSpeed(true, size) << ")";
+      return;
+    }
+    catch (Orthanc::OrthancException &ex)
+    {
+      LOG(INFO) << "Writing file caught exception: " << ex.What();
+      if (retryCount >= maxRetryCount)
+      {
+        throw ex;
+      }
     }
   }
-  else
-  {
-    if (!boost::filesystem::create_directories(mount_path.parent_path()))
-    {
-      LOG(ERROR) << "[SaolaStorageArea] Mount directory: " << mount_path.parent_path() << " cannot be created";
-      // throw Orthanc::OrthancException(Orthanc::ErrorCode_FileStorageCannotWrite);
-    }
-  }
-
-  Orthanc::SystemToolbox::WriteFile(mount_path.string().c_str(), mount_path.string().size(), root_path.string(), false);
-
-  Orthanc::SystemToolbox::WriteFile(content, size, mount_path.string(), false);
+  // LOG(INFO) << "StorageArea::Create created attachment \"" << uuid << "\" (" << timer.GetHumanElapsedDuration() << ")";
 
   // boost::system::error_code se;
 
@@ -278,37 +338,52 @@ void StorageArea::Create(const std::string &uuid,
 void StorageArea::ReadWhole(std::string &target,
                             const std::string &uuid)
 {
+  Orthanc::Toolbox::ElapsedTimer timer;
+  LOG(INFO) << "StorageArea::ReadWhole string attachment \"" << uuid << "\"";
+
   std::string floc;
   Orthanc::SystemToolbox::ReadFile(floc, GetPathInternal(root_, uuid).string());
   Orthanc::SystemToolbox::ReadFile(target, floc);
+
+  LOG(INFO) << "StorageArea::ReadWhole string attachment \"" << uuid << "\" (" << timer.GetHumanTransferSpeed(true, target.size()) << ")";
 }
 
 void StorageArea::ReadWhole(OrthancPluginMemoryBuffer64 *target,
                             const std::string &uuid)
 {
+  Orthanc::Toolbox::ElapsedTimer timer;
+  LOG(INFO) << "StorageArea::ReadWhole OrthancPluginMemoryBuffer64 attachment \"" << uuid << "\"";
+
   std::string floc;
   Orthanc::SystemToolbox::ReadFile(floc, GetPathInternal(root_, uuid).string());
 
   ReadWholeFromPath(target, floc);
+  LOG(INFO) << "StorageArea::ReadWhole OrthancPluginMemoryBuffer64 attachment \"" << uuid << "\" (" << timer.GetHumanTransferSpeed(true, target->size) << ")";
 }
 
 void StorageArea::ReadRange(OrthancPluginMemoryBuffer64 *target,
                             const std::string &uuid,
                             uint64_t rangeStart)
 {
-
+  Orthanc::Toolbox::ElapsedTimer timer;
+  LOG(INFO) << "StorageArea::ReadRange attachment \"" << uuid << "\" content type (range from: " << rangeStart << ")";
   std::string floc;
   Orthanc::SystemToolbox::ReadFile(floc, GetPathInternal(root_, uuid).string());
   ReadRangeFromPath(target, floc, rangeStart);
+
+  LOG(INFO) << "StorageArea::ReadRange attachment \"" << uuid << "\" (" << timer.GetHumanTransferSpeed(true, target->size) << ")";
 }
 
 void StorageArea::RemoveAttachment(const std::string &uuid)
 {
+  Orthanc::Toolbox::ElapsedTimer timer;
+  LOG(INFO) << "StorageArea::RemoveAttachment deleting attachment \"" << uuid << "\"";
+
   boost::filesystem::path root_path = GetPathInternal(root_, uuid);
 
   std::string floc;
   Orthanc::SystemToolbox::ReadFile(floc, root_path.string());
-  boost::filesystem::path mount_path  = floc;
+  boost::filesystem::path mount_path = floc;
 
   try
   {
@@ -325,6 +400,7 @@ void StorageArea::RemoveAttachment(const std::string &uuid)
   {
     // Ignore the error
   }
+  LOG(INFO) << "StorageArea::RemoveAttachment deleted attachment \"" << uuid << "\" (" << timer.GetHumanElapsedDuration() << ")";
 }
 
 std::string StorageArea::GetPath(const std::string &uuid) const
